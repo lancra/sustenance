5cfc556fcd6b2f9207cf65abe524e036945a8287
Lance Craig <lancra@outlook.com>
2024-02-13 06:13:00 -0500

    Add test target

--- tools/Dev/ArtifactPaths.cs
+++ tools/Dev/ArtifactPaths.cs
@@ -1,11 +1,19 @@
+using System.Diagnostics.CodeAnalysis;
 using System.Text;
 
 namespace Sustenance.Dev;
 
+[SuppressMessage(
+    "StyleCop.CSharp.OrderingRules",
+    "SA1203:Constants should appear before fields",
+    Justification = "Static formats are grouped with their related constants.")]
 internal static class ArtifactPaths
 {
     public const string Root = "artifacts";
 
     public const string PublishedExecutables = $"{Root}/publish";
     public static readonly CompositeFormat PublishedExecutableFormat = CompositeFormat.Parse($"{PublishedExecutables}/{{0}}");
+
+    public const string TestResults = $"{Root}/test-results";
+    public static readonly CompositeFormat TestResultFormat = CompositeFormat.Parse($"{TestResults}/{{0}}");
 }
